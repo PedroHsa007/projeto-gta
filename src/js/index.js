a40b5417a709b04636ef923b3d72043f467d84d0
PedroHsa007 <141883909+PedroHsa007@users.noreply.github.com>
2024-01-28 00:01:11 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+
+const botao = document.querySelector(".btn-plataforma");
+
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+botao.addEventListener("click",() => {
+
+    const botaoEstaAberto = elementoPlataformas.classList.
+    contains("ativo");
+
+    if(botaoEstaAberto){
+        elementoPlataformas.classList.remove("ativo");
+    }else{
+        elementoPlataformas.classList.add("ativo");
+    }
+})
